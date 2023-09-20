fd6d09667077f87bf84669fbc7ee939c41372293
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-20 14:55:38 -0400

    Revert "Revert "need to add spaces, kinda starting it""
    This reverts commit bfdbf3b14012481e3a1f789027b7713e5028f4a0.

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,11 +1,46 @@
 class PascalTriangle {
-    public static void main(String[] args) {
-        
+
+    // run the pascal
+    public static void printPascal(int n) {
+        // spacing
+        for (int line = 1; line <= n; line++) {
+            for (int j = 0; j <= n - line; j++) {
+
+                // spacing
+                System.out.print("  ");
+            }
+
+			// used to represent C (line, i)
+			int choose = 1;
+			for (int i = 1; i <= line; i++) {
+
+                // The first value in a line is always 1
+                System.out.print(choose + "   ");
+                choose = choose * (line - i) / i;
+            }
+            System.out.println();
+        }
     }
 
-    public static int factorial(int i) {
-        
-        
-        return i;
-    }
+    public static void main(String[] args) {
+        // initialize n var
+        int n;
+
+        // while loop to repeat input
+        while (true) {
+            System.out.println("How many rows do you want to print out (max 20, -1 to exit)?");
+            n = In.getInt();
+
+            // if over 20, or break
+            if (n > 20) {
+                System.out.println("That number is over 20, please try again.");
+            } else if (n == -1){
+                System.out.println("Exiting...");
+                break;
+            } else {
+                // method
+                printPascal(n);
+            }
+        }
+	}
 }
