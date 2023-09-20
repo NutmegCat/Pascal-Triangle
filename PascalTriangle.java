90c9742b3208da8db2af72e96516259e5b050f06
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-20 11:38:47 -0400

    commit doesnt work, rows go -ve after 15

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,17 +1,21 @@
 // Print Pascal's Triangle in Java
 
 class PascalTriangle {
-	public int factorial(int i)
-	{
+	
+    public int factorial(int i) {
 		if (i == 0)
 			return 1;
 		return i * factorial(i - 1);
 	}
-	public static void main(String[] args)
-	{
-		int n = 4, i, j;
+	public static void main(String[] args){
+
+        // System.out.println("How many rows would you like to print?");
+        // int x = In.getInt();
+		
+        int n = 15, i, j;
 		PascalTriangle g = new PascalTriangle();
-		for (i = 0; i <= n; i++) {
+		
+        for (i = 0; i <= n; i++) {
 			for (j = 0; j <= n - i; j++) {
 
 				// for left spacing
