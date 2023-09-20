4ee1cd7da3671d0ba3a9e7ff538fbfdf08dc6b85
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-20 11:53:42 -0400

    final

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,38 +1,49 @@
-// Print Pascal's Triangle in Java
+// Aaron Prince Anu
 
 class PascalTriangle {
-	
-    public int factorial(int i) {
-		if (i == 0)
-			return 1;
-		return i * factorial(i - 1);
-	}
-	public static void main(String[] args){
 
-        // System.out.println("How many rows would you like to print?");
-        // int x = In.getInt();
-		
-        int n = 15, i, j;
-		PascalTriangle g = new PascalTriangle();
-		
-        for (i = 0; i <= n; i++) {
-			for (j = 0; j <= n - i; j++) {
+	// run the pascal
+	public static void printPascal(int n)
+	{
+        //spacing
+		for (int line = 1; line <= n; line++) {
+			for (int j = 0; j <= n - line; j++) {
 
-				// for left spacing
+				// spacing
 				System.out.print(" ");
 			}
-			for (j = 0; j <= i; j++) {
 
-				// nCr formula
-				System.out.print(
-					" "
-					+ g.factorial(i)
-						/ (g.factorial(i - j)
-							* g.factorial(j)));
+			// used to represent C (line, i)
+			int choose = 1;
+			for (int i = 1; i <= line; i++) {
+
+				// The first value in a line is always 1
+				System.out.print(choose + " ");
+				choose = choose * (line - i) / i;
 			}
-
-			// for newline
 			System.out.println();
 		}
+	}
+	public static void main(String[] args)
+	{
+        // initialize n var
+        int n;
+
+        // while loop to repeat input
+        while (true) {
+            System.out.println("How many rows do you want to print out (max 20, -1 to exit)?");
+            n = In.getInt();
+
+            // if over 20, or break
+            if (n > 20) {
+                System.out.println("That number is over 20, please try again.");
+            } else if (n == -1){
+                System.out.println("Exiting...");
+                break;
+            } else {
+                // method
+                printPascal(n);
+            }
+        }
 	}
 }
