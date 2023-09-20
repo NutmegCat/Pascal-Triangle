6b8d691a6e736c056bb9986263d1f2aeb1fb4c1d
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-20 17:58:08 -0400

    Revert "need to add spaces, kinda starting it"
    This reverts commit 8e6c5e26434bcca5300f8583e33f5b57caadeaa6.

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -2,29 +2,30 @@
 
 class PascalTriangle {
 
-    // run the pascal
-    public static void printPascal(int n) {
-        // spacing
-        for (int line = 1; line <= n; line++) {
-            for (int j = 0; j <= n - line; j++) {
+	// run the pascal
+	public static void printPascal(int n)
+	{
+        //spacing
+		for (int line = 1; line <= n; line++) {
+			for (int j = 0; j <= n - line; j++) {
 
-                // spacing
-                System.out.print("  ");
-            }
+				// spacing
+				System.out.print(" ");
+			}
 
 			// used to represent C (line, i)
 			int choose = 1;
 			for (int i = 1; i <= line; i++) {
 
-                // The first value in a line is always 1
-                System.out.print(choose + "   ");
-                choose = choose * (line - i) / i;
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
+				// The first value in a line is always 1
+				System.out.print(choose + " ");
+				choose = choose * (line - i) / i;
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args)
+	{
         // initialize n var
         int n;
 
