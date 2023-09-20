bb9f0ae50e836737c474db60e39d20dbd833ba19
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-20 11:30:52 -0400

    an iteration (doesnt work)

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,11 +1,34 @@
 class PascalTriangle {
-    public static void main(String[] args) {
-        
+
+    static int factorial(int n) {
+
+        int f;
+
+        for (f = 1; n > 1; n--) {
+            f *=n;
+        }
+        return f;
     }
 
-    public static int factorial(int i) {
-        
-        
-        return i;
+    static int ncr (int n, int r) {
+        return factorial(n) / (factorial(n - 1) * factorial(r));
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        int n;
+        int i;
+        int j;
+        n = 5;
+
+        for (i = 0; i <= n; i++) {
+            for (j = 0; j <= n; j++) {
+                System.out.println(" ");
+            }
+            for (j = 0; j <= n; j++) {
+                System.out.println(" " + ncr(i, j));
+            }
+            System.out.println();
+        }
     }
 }
